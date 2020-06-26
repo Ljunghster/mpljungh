8ee6ed2595612b9fd7e0115af38d5ccaa88a87f5
Ljunghster <mpljungh@gmail.com>
2020-06-25 22:21:29 -0700

    still trying to get things to display, logo is pain

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import { Button } from 'reactstrap';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import {
+  Switch,
+  Link,
+  Route,
+  BrowserRouter as Router
+} from 'react-router-dom'
+import PageOne from './pages/PageOne'
+import PageTwo from './pages/PageTwo'
+import CountContext from './utils/CountContext'
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  Button
+} from 'reactstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    count: 0,
+    handleIncrement: () => {
+      this.setState({ count: this.state.count + 1 })
+    },
+    handleDecrement: () => {
+      this.setState({ count: this.state.count - 1 })
+    }
+  }
+
+  render() {
+    return (
+      <CountContext.Provider value={this.state}>
+        <Router>
+          <div>
+            <Navbar color="light">
+              <NavbarBrand>
+                <Link to="/" className="navBrand">
+                  My App
+                </Link>
+              </NavbarBrand>
+              <Nav>
+                <NavItem>
+                  <Button color="info">
+                    <Link to="/" className="navLinks">
+                      Page One
+                    </Link>
+                  </Button>
+                </NavItem>
+                <NavItem>
+                  <Button color="info">
+                    <Link to="/pagetwo" className="navLinks">
+                      Page Two
+                    </Link>
+                  </Button>
+                </NavItem>
+              </Nav>
+            </Navbar>
+            <Switch>
+
+              <Route exact path="/">
+                <PageOne />
+              </Route>
+              <Route path="/pagetwo">
+                <PageTwo />
+              </Route>
+
+            </Switch>
+          </div>
+        </Router>
+      </CountContext.Provider>
+    )
+  }
 }
 
-export default App;
+export default App
